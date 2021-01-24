45b56112ed87859529206c205230452aec57bb7a
Kelyn Crandall <kelyn@crandall.me>
2021-01-24 17:47:56 -0600

    Added test physics sim

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Henelik/tricaster/physics"
+	"github.com/Henelik/tricaster/tuple"
+)
+
 func main() {
-	return
+	physics_test()
+}
+
+func physics_test() {
+	// projectile starts one unit above the origin
+	// velocity is normalized to 1  unit/tick
+	p, err := physics.CreateProjectile(
+		tuple.NewPoint(0, 0, 1),
+		tuple.NewVector(1, 0, 1),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// gravity -0.1 unit/tick, and wind is -0.01 unit/tick
+	e, err := physics.CreateEnvironment(
+		tuple.NewVector(0, 0, -0.1),
+		tuple.NewVector(-0.01, 0, 0),
+	)
+
+	for p.Pos.Z > 0 {
+		p = p.Tick(e)
+		log.Printf("projectile position: X: %f, Y: %f, Z:%f, W:%f\n", p.Pos.X, p.Pos.Y, p.Pos.Z, p.Pos.W)
+	}
 }
---
+++ physics/environment.go
@@ -0,0 +1,22 @@
+package physics
+
+import (
+	"errors"
+
+	"github.com/Henelik/tricaster/tuple"
+)
+
+type Environment struct {
+	Gravity *tuple.Tuple
+	Wind    *tuple.Tuple
+}
+
+func CreateEnvironment(grav, wind *tuple.Tuple) (*Environment, error) {
+	if !grav.IsVector() {
+		return nil, errors.New("grav must be a vector")
+	}
+	if !wind.IsVector() {
+		return nil, errors.New("wind must be a vector")
+	}
+	return &Environment{grav, wind}, nil
+}
