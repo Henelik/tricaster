ea55a01943996a809b03526314b2187616f0159e
Kelyn Crandall <kelyn@crandall.me>
2021-01-27 06:38:38 -0600

    Store inverse matrix on sphere to make rendering 100x faster

--- geometry/sphere.go
+++ geometry/sphere.go
@@ -9,15 +9,22 @@ import (
 )
 
 type Sphere struct {
+	// the transformation matrix
 	M *matrix.Matrix
+	// the inverse transformation matrix
+	im *matrix.Matrix
 }
 
 func NewSphere(m *matrix.Matrix) *Sphere {
-	return &Sphere{m}
+	return &Sphere{m, m.Inverse()}
+}
+
+func (s *Sphere) UpdateIM() {
+	s.im = s.M.Inverse()
 }
 
 func (s *Sphere) Intersects(r *ray.Ray) []Intersection {
-	rt := r.Transform(s.M.Inverse())
+	rt := r.Transform(s.im)
 	sphereToRay := rt.Origin.Sub(tuple.NewPoint(0, 0, 0))
 	a := rt.Direction.DotProd(rt.Direction)
 	b := 2 * rt.Direction.DotProd(sphereToRay)
