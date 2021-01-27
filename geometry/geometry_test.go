e1943951df67095acec38328faf1f15540363c78
Kelyn Crandall <kelyn@crandall.me>
2021-01-27 06:25:30 -0600

    Added sphere intersection benchmark

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/Henelik/tricaster/matrix"
+	"github.com/Henelik/tricaster/ray"
+	"github.com/Henelik/tricaster/tuple"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -53,5 +55,36 @@ func TestHit(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			assert.Equal(t, tc.want, Hit(tc.inters))
 		})
+	}
+}
+
+func BenchmarkIntersection128(b *testing.B) {
+	var hit bool
+	for n := 0; n < b.N; n++ {
+		s := NewSphere(matrix.Translation(0, 5, 0))
+
+		w := 128
+		h := 128
+
+		for x := 0; x < w; x++ {
+			for y := 0; y < h; y++ {
+				// shoot a ray at the sphere
+				xPos := (float64(x) - float64(w)/2.0) / (float64(w) * 0.4)
+				yPos := -(float64(y) - float64(h)/2.0) / (float64(h) * 0.4)
+				r := ray.NewRay(
+					tuple.NewPoint(xPos, 0, yPos),
+					tuple.NewVector(0, 1, 0),
+				)
+				h := Hit(s.Intersects(r))
+				if h != NilHit {
+					hit = true
+				} else {
+					hit = false
+				}
+			}
+		}
+	}
+	if hit {
+		return
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Henelik/tricaster
 
 go 1.15
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/perf v0.0.0-20201207232921-bdcc6220ee90 // indirect
+)
